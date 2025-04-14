2eb041cbc6749db2985b5230122f2a5f04632c0d
Satrio <personal.satrio@protonmail.com>
2025-04-14 22:37:19 +0700

    added note

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,7 +9,7 @@ import (
 
 func Config(key string) string {
 
-	// load .env file
+	// load .env file with godotenv might be problematic with docker, bcuz docker use real enviroment variable
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Print("Error loading .env file")
